0716b5e20b5088dc418729ffee557e3d23976fe2
nicoolle <nikakozmanko@gmail.com>
2021-10-05 22:04:17 +0300

    Added some methods

--- lib/todo_list.dart
+++ lib/todo_list.dart
@@ -1,22 +1,40 @@
+import 'package:flutter/foundation.dart';
 import 'package:path/path.dart';
-import 'package:sqflite/sqflite.dart';
-
+import "package:collection/collection.dart";
 import 'models/task.dart';
 
 class ToDoList {
-  Future<Database> database() async {
-    return openDatabase(
-      join(await getDatabasesPath(), 'todo.db'),
-      onCreate: (db, version) {
-        return db.execute(
-          'CREATE TABLE tasks(id INTEGER PRIMARY KEY, title TEXT, description TEXT, category TEXT)',
-        );
-      },
-    );
+  List<Task> tasks = <Task>[];
+  List<Category> categories = <Category>[];
+
+
+  void addTask(List<Task> tasksToAdd){// List<Category> categoriesToAdd) {
+    tasks.addAll(tasksToAdd);
+    /*if(categories == categoriesToAdd) {
+      categories.addAll(categoriesToAdd);
+    }*/
   }
 
-  Future<void> insertTask(Task task) async {
-    Database _db = database() as Database;
-    await _db.insert('tasks', task.toMap());
+  void deleteTask(int id){
+    tasks.removeWhere((element) => element.id == id);
+  }
+
+  List<Task> getAll() {
+    return tasks;
+  }
+
+  void setCategory(String categoryToAdd) {
+    tasks.add(categoryToAdd);
+  }
+
+  void setDayOfWeek(){
+    if(tasks. is RecurringTask){
+
+    }
+  }
+
+  Map<String, int> getCategories() {
+    var categories = groupBy(tasks, (Task task) => task.category).map((k, v) => MapEntry(k, v.length));
+    return categories;
   }
 }
